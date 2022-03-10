c986277ccbefb3ea2242812eb50ed16619635521
unknown <tom@dlnet.us>
2022-03-10 16:50:15 +0100

    color-coding

--- root/js/site.js
+++ root/js/site.js
@@ -5,22 +5,16 @@
     return data;
 }
 
-function onfail(ex) {
-    alert("Failed to load data");
-
-    console.error(ex);
-}
-
-function itemToNode(item) {
+function createNode(item) {
     let elItem = document.createElement("div");
     elItem.className = "item center";
 
     let elName = document.createElement("p");
-    elName.className = "item-name";
+    elName.className = "track-title";
     elName.innerText = item.name;
 
     let elID = document.createElement("p");
-    elID.className = "item-id";
+    elID.className = "track-id";
     elID.innerText = item.id;
 
     elItem.appendChild(elName);
@@ -30,24 +24,32 @@ function itemToNode(item) {
 }
 
 async function load() {
-    try {
-        let data = await pull();
-        let json = JSON.parse(data);
-        let list = document.getElementsByClassName("list")[0];
-        let msg = document.getElementsByClassName("message")[0];
+    let data = await pull();
+    let json = JSON.parse(data);
 
-        msg.innerText = json.message;
+    let msg = document.getElementsByClassName("message")[0];
+    let list = document.getElementsByClassName("list")[0];
+    let splitter = document.getElementsByClassName("splitter")[0];
 
-        for (let i = 0; i < json.data.length; i++) {
-            let item = json.data[i];
-            let elItem = itemToNode(item);
+    splitter.innerText = "- (" + json.data.length + ") -";
+    msg.innerText = json.message;
 
-            list.appendChild(elItem);
-        }
-    }
-    catch (ex) {
-        onfail(ex);
+    for (let i = 0; i < json.data.length; i++) {
+        let item = json.data[i];
+        let elItem = createNode(item);
+
+        list.appendChild(elItem);
     }
 }
+
+/*
+let msg = document.getElementsByClassName("message")[0];
+let list = document.getElementsByClassName("list")[0];
+let splitter = document.getElementsByClassName("splitter")[0];
+
+splitter.innerText = "- (" + 1 + ") -";
+let elItem = createNode({ name: "test - test", id: 9 });
+msg.innerText = "These are maps that will be put in Sound Space!"
+list.appendChild(elItem);*/
 
 load();
