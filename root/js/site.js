6c2b4c8c416842a909aac703231ccb6df944dcba
unknown <tom@dlnet.us>
2022-03-10 15:09:40 +0100

    stuff

---
+++ root/js/site.js
@@ -0,0 +1,24 @@
+﻿async function downloadFile() {
+    let response = await fetch("./root/data.txt");
+
+    if (response.status != 200) {
+        throw new Error("Server Error");
+    }
+
+    let text_data = await response.text();
+
+    return text_data;
+}
+
+async function loadData() {
+    try {
+        let text_data = await downloadFile();
+
+        console.log(text_data);
+    }
+    catch (e) {
+        alert(e.message);
+    }
+}
+
+loadData();
